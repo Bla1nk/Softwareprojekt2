36cb7d96424b35150f72f1901df506a1406f1201
matsadel <102972635+matsadel@users.noreply.github.com>
2023-05-05 17:27:20 +0200

    Add files via upload

---
+++ csv_umwandeln.py
@@ -0,0 +1,27 @@
+import csv
+
+csv_file_path = "/Users/matsadel/Desktop/Software Projekt 2/mySketch/bin/data/kunstoffproduktion.csv"
+
+# öffnet csv datei und liest daten aus
+with open(csv_file_path, "r", newline="") as csv_file:
+    reader = csv.reader(csv_file, delimiter=",")
+    data = list(reader)
+
+# durchläuft zeilen und ersetzt jeweils das , mit einem .
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        # überprüft die Zahlen auf dezimal stellen und ändert dann das Komma zu einem Punkt
+        if isinstance(data[i][j], str) and "," in data[i][j] and data[i][j].replace(",", "").replace(".", "").isdigit():
+    
+            data[i][j] = data[i][j].replace(",", ".")
+'''for i in range(len(data)):
+    data[i] = data[i][:2] + data[i][3:]'''
+
+# neue Daten werden in csv datei geschrieben mit einem , als neuen separator
+with open(csv_file_path, "w", newline="") as csv_file:
+    writer = csv.writer(csv_file, delimiter=",")
+    writer.writerows(data)
+
+for row in data:
+    print(row)
+print("Die Änderungen wurden erfolgreich in der CSV-Datei gespeichert!")
